8c8721d7eaae9c7dce003eb39d9dc21051661698
modmac1000gmail-com <modmac1000@gmail.com>
2025-09-07 16:28:41 +0300

    Add Firebase configuration

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBo3NYLUxFTUjjkyFdZOCgXGNRfobXGiCM",
+  authDomain: "rojer-7ab7f.firebaseapp.com",
+  projectId: "rojer-7ab7f",
+  storageBucket: "rojer-7ab7f.firebasestorage.app",
+  messagingSenderId: "978537605589",
+  appId: "1:978537605589:web:1dc7bca9c59f828227f55c",
+  measurementId: "G-Z4PF2GYPD7"
+};
